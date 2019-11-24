b33610cd13100ce5ed4fc8ba80b82d1d38d90490
Bijoy <mv.bijoy@gmail.com>
2019-11-24 18:50:58 +0100

    Adding App components and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Container, Col, Row } from 'react-bootstrap';
+import Sidemenu from './components/Sidemenu';
+import Header from './components/Header';
+import Main from './components/Main';
+import Bottommenu from './components/Bottommenu';
 
 function App() {
-  return <div className="App">Hi</div>;
+  const [msg, setMsg] = useState('');
+
+  function handleBtnClick(e) {
+    if (e.target.name === 'dropdown') {
+      console.log(`User selected the dropdown option : ${e.target.value}`);
+      setMsg(e.target.value);
+    } else if (e.target.type === 'checkbox') {
+      if (e.target.checked) {
+        console.log(`User clicked the selection : ${e.target.name}`);
+        setMsg(e.target.name);
+      } else {
+        console.log(`User removed the selection : ${e.target.name}`);
+        setMsg('');
+      }
+    } else {
+      console.log(`User clicked the button : ${e.target.name}`);
+      setMsg(e.target.name);
+    }
+  }
+  return (
+    <React.Fragment>
+      <Row className="header">
+        <Header handleClick={handleBtnClick} />
+      </Row>
+      <Container className="wrapper">
+        <Row className="grow py-3">
+          <Sidemenu handleClick={handleBtnClick} />
+          <Col xs={12} md={8} className="py-3">
+            <Main msg={msg} />
+            <Bottommenu handleClick={handleBtnClick} />
+          </Col>
+        </Row>
+      </Container>
+    </React.Fragment>
+  );
 }
 
 export default App;
