7bbe8bba11d4a619abac5e4ffd08913b45e0e652
Bijoy <mv.bijoy@gmail.com>
2019-11-24 18:10:15 +0100

    Adding Main components

---
+++ src/components/Main/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import videoPlaceholder from './../../assets/iccms.png';
+import './style.css';
+
+const Main = ({ msg }) => {
+  let hideImg = '';
+  let hideText = 'hidden';
+  if (msg === '') {
+    hideImg = '';
+    hideText = 'hidden';
+  } else {
+    hideImg = 'hidden';
+    hideText = '';
+  }
+  return (
+    <React.Fragment>
+      <div className={`img-container ${hideImg}`}>
+        <img
+          src={videoPlaceholder}
+          alt="video place holder"
+          className="img-fluid"
+        />
+      </div>
+      <div className={`img-container ${hideText}`}>
+        <div className=" img-text ">
+          User clicked the button : <b>{msg}</b>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Main;
